1759f38a3efa5b747e2f24521a8b1ca9aa2dca54
Oscar Zhu <oscar@yvrmail.net>
2022-05-10 19:53:47 -0700

    created SQL basic module

--- public/js/sql.js
+++ public/js/sql.js
@@ -1,33 +1,60 @@
 const mysql = require('mysql');
 
 
-const con = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
+class SQL {
+    initConnection() {
+        this.con = mysql.createConnection({
+            host: process.env.HOST,
+            user: process.env.USER,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE
+        });
 
-con.connect(err => {
-    if (err) throw err;
-    console.log('Connected')
+        this.con.connect(err => {
+            if (err) throw err;
+            console.log('Connected to database:', this.con.config.database)
+        })
+    };
 
-    const createTableQuery = [
-        'CREATE TABLE IF NOT EXISTS users',
-        '(id INT AUTO_INCREMENT PRIMARY KEY,',
-        'email VARCHAR(255),',
-        'password VARCHAR(255),',
-        'admin BOOL)'
-    ].join(' ');
+    end() {
+        this.con.end(err => {
+            if (err) throw err;
+            console.log('database connection closed!');
+            process.exit();
+        });
+    };
 
-    con.query(createTableQuery, (err, result) => {
-        if (err) throw err;
-        console.log('Table created!');
-    });
+    createTable() {
+        const createTableQuery = [
+            'CREATE TABLE IF NOT EXISTS users',
+            '(id INT AUTO_INCREMENT PRIMARY KEY,',
+            'username CHAR(20),',
+            'email VARCHAR(255),',
+            'password VARCHAR(255))'
+        ].join(' ');
+    
+        this.con.query(createTableQuery, (err, result) => {
+            if (err) throw err;
+            console.log('Table created!', result);
+        });
+    };
 
-    con.end(err => {
-        if (err) throw err;
-        console.log('database connection closed!');
-        process.exit();
-    });
-});
+    readUsers() {
+        this.con.query('SELECT * FROM users', (err, result, fields) => {
+            if (err) throw err;
+            console.log(result)
+            console.log(fields)
+        })
+    };
+
+    addNewUser(username, email, password) {
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES('${username}', '${email}', '${password}')`;
+        this.con.query(insertQuery, (err, result) => {
+            if (err) throw err;
+            console.log('User added')
+        })
+    };
+};
+
+
+module.exports = SQL;
--- server.js
+++ server.js
@@ -1,93 +1,97 @@
+const SQL = require('./public/js/sql.js');
 const https = require("https");
 const bodyParser = require("body-parser");
 const express = require("express");
 const session = require("express-session");
 const app = express();
+const sql = new SQL();
 
 
 users = {
-  user1: { password: "123", email: "user1@gmail.com" },
-  user2: { password: "456", email: "user2@gmail.com" },
-  user3: { password: "789", email: "user3@gmail.com" },
+	user1: { password: "123", email: "user1@gmail.com" },
+	user2: { password: "456", email: "user2@gmail.com" },
+	user3: { password: "789", email: "user3@gmail.com" },
 };
 
 app.use(
-  session({
-    secret: "I ain't tellin' nobody!",
-    saveUninitialized: true,
-    resave: true,
-  })
+	session({
+		secret: "I ain't tellin' nobody!",
+		saveUninitialized: true,
+		resave: true,
+	})
 );
 
 app.use(express.static("./public"));
 
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+	bodyParser.urlencoded({
+		extended: true,
+	})
 );
 
 app.set("view engine", "ejs");
 
 app.listen(5001, function (err) {
-  if (err) console.log(err);
+	if (err) console.log(err);
+	console.log('Listening')
 });
 
 app.get("/", function (req, res) {
-  if (req.session.authenticated) {
-    res.sendFile(__dirname + "/public/main.html");
-  } else {
-    res.sendFile(__dirname + "/public/login.html");
-  }
+	if (req.session.authenticated) {
+		res.sendFile(__dirname + "/public/main.html");
+	} else {
+		res.sendFile(__dirname + "/public/login.html");
+	}
 });
 
 app.get("/login", function (req, res) {
-  res.sendFile(__dirname + "/public/login.html");
+	res.sendFile(__dirname + "/public/login.html");
 });
 
 app.get("/logout", function (req, res) {
-  req.session.authenticated = false;
-  // diplay "logged out" via alert or temporary page
-  res.redirect("/");
+	req.session.authenticated = false;
+	// diplay "logged out" via alert or temporary page
+	res.redirect("/");
 });
 
 // use "/loginAttempt" to use AJAX request
 app.post("/auth", function (req, res) {
-  username = req.body.username;
-  password = req.body.password;
-  // username;
-  console.log(username + " stores " + password);
-  if (username == "admin" && password == "topsecret") {
-    req.session.admin = true;
-  } else if (username in users && users[username].password == password) {
-    req.session.authenticated = true;
-    req.session.admin = false;
-    req.session.user = req.params.username;
-    res.redirect("/");
-  } else {
-    req.session.authenticated = false;
-    req.session.admin = false;
-    res.send(`Password for ${req.session.user} is incorrect`);
-  }
+	username = req.body.username;
+	password = req.body.password;
+	// username;
+	console.log(username + " stores " + password);
+	if (username == "admin" && password == "topsecret") {
+		req.session.admin = true;
+	} else if (username in users && users[username].password == password) {
+		req.session.authenticated = true;
+		req.session.admin = false;
+		req.session.user = req.params.username;
+		res.redirect("/");
+	} else {
+		req.session.authenticated = false;
+		req.session.admin = false;
+		res.send(`Password for ${req.session.user} is incorrect`);
+	}
 });
 
 app.post("/admin", function (req, res) {
-  if (req.session.admin) {
-    res.send(user);
-  } else {
-    res.send("Unauthorized - access denied");
-  }
+	if (req.session.admin) {
+		res.send(user);
+	} else {
+		res.send("Unauthorized - access denied");
+	}
 });
 
 app.post("/register", function (req, res) {
-  username = req.body.username;
-  password = req.body.password;
-  email = req.body.email;
-  if (username in users) {
-    res.send("Username already exists");
-  } else {
-    console.log("update users");
-  }
+	const username = req.body.username,
+		  password = req.body.password,
+		  email = req.body.email;
+
+	if (username in users) res.send("Username already exists");	
 });
 
-require('./public/js/sql.js')
+
+// sql.initConnection();
+// sql.createTable();
+// sql.addNewUser('user1', 'user1@gmail.com', 'password123');
+// sql.readUsers();
