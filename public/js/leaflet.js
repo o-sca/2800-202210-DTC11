7f13a5ce5047c2fb9b5ab0e2fe0366264b7143eb
Oscar Zhu <oscar@yvrmail.net>
2022-05-16 18:16:45 -0700

    leaflet: added a simple current location event listener

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -3,10 +3,10 @@ function createMap(object) {
     const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     const tiles = L.tileLayer(tileURL, { attribution });
 
-    var map = L.map('map').setView([object.lat, object.long], 13)
-    var userMarker =  L.marker([object.lat, object.long]).addTo(map);
+    var map = L.map('map').setView([object.lat, object.lng], 13)
 
     tiles.addTo(map);
+    return map;
 };
 
 async function getUserLocation() {
@@ -15,19 +15,67 @@ async function getUserLocation() {
         navigator.geolocation.getCurrentPosition(function (showPosition) {
             return resolve({
                 lat: showPosition.coords.latitude,
-                long: showPosition.coords.longitude
+                lng: showPosition.coords.longitude
             });
         });
     });
 };
 
-function userMarker() {
-    return
+function userMarker (object, map) {
+    const userMarker = L.marker([object.lat, object.lng]).addTo(map);
+    return userMarker;
 };
 
+function round(number) {
+    return Math.round(number * 1000) / 1000;
+};
 
 (async () => {
-    let response = await getUserLocation();
-    if (response === false) return alert(`Please enable current locations`);
-    createMap(response);
+    let location = await getUserLocation();
+    let map = createMap(location);
+    
+    let user = userMarker(location, map);
+
+    map.on('click', e => {
+        if (round(e.latlng.lng) == round(user._latlng.lng)) {
+            console.log('You are here!')
+        }
+    });
+
+
 })();
+
+class Leaflet {
+    createMap() {
+        const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+        const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        const tiles = L.tileLayer(tileURL, { attribution });
+
+        this.map = L.map('map').setView([this.lat, this.long], 13)
+        tiles.addTo(map);
+    };
+
+    async getUserLocation() {
+        return new Promise(async (resolve, reject) => {
+            if (!navigator.geolocation) return reject(false); // Geolocation not supported;
+            navigator.geolocation.getCurrentPosition(function (showPosition) {
+                this.lat = showPosition.coords.latitude,
+                this.long = showPosition.coords.longitude
+                return resolve();
+            });
+        });
+    };
+
+    userMarker() {
+        return L.marker([this.lat, this.long]).addTo(this.map)
+    };
+
+};
+
+// (async () => {
+//     const leaflet = new Leaflet();
+    
+//     await leaflet.getUserLocation();
+//     leaflet.createMap();
+
+// })();
