ab1edce1d1a1a4c31123c663f53af6fd018d3bb1
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-05-18 14:32:24 -0700

    Created filterStations() that returns lat/lng array

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,5 +1,23 @@
-async function filterStations() {
-  // let stations = await fetchStation();
-  map.setView([49.270056, -123.061295], 12);
+async function filterStations(params = {}) {
+  let filteredStations = await fetchStation();
+  if (!filteredStations.length) return false;
+  if (!params) return filteredStations;
+  console.log(filteredStations[0]);
+  const { stationsCount, kilowatts, power, status } = params;
+  filteredStations = filteredStations.filter((station) => {
+    let { address, lat, lng, stations } = station;
+    let pass = true;
+    if (stationsCount && stationsCount > stations.length) pass = false;
+    return pass;
+  });
+  coords = filteredStations.map((station) => {
+    return { lat: station.lat, lng: station.lng };
+  });
+  console.log(coords);
+  return filteredStations;
 }
-// console.log(map);
+
+// function filterBy
+
+let filteredStations = filterStations({ stationsCount: 6 });
+// populateStations(data, map);
