0a91c4516d074d5eac512606284e4fe838b729d7
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-05-19 18:38:48 -0700

    Added slider for kilowatts and filters styling

--- public/js/filter.js
+++ public/js/filter.js
@@ -3,15 +3,21 @@ async function filterStations(params = {}) {
   // console.log(STATIONS[0]);
   if (!STATIONS.length) return false;
   if (!params) return STATIONS;
-  const { numOfStations, kilowatts, power, status, availability } = params;
+  const { numOfStations, kilowatts, power, status, availability, searchTerm } =
+    params;
   const filteredStations = STATIONS.filter((STATION) => {
-    const { stations } = STATION;
+    const { name, address, stations } = STATION;
     const stationData = getStationInnerData(stations);
     if (numOfStations && numOfStations > stations.length) return false;
-    if (kilowatts && !stationData.kilowatts.includes(kilowatts)) return false;
+    if (kilowatts && kilowatts > Math.max(stationData.kilowatts)) return false;
     if (power && !stationData.power.includes(power)) return false;
     if (status && !stationData.status.includes(status)) return false;
     if (availability && availability > getAvailability(stationData.status))
+      return false;
+    if (
+      searchTerm &&
+      (!name.includes(searchTerm) || !address.includes(searchTerm))
+    )
       return false;
     return true;
   });
@@ -26,7 +32,6 @@ async function searchStations(query = "") {
     const { name, address } = station;
     if (name.includes(query) || address.includes(query)) return true;
   });
-  // console.log(toLatLng(filteredStations));
   return toLatLng(filteredStations);
 }
 
@@ -76,8 +81,29 @@ async function stationDataSets() {
       });
     });
   });
-  // console.log({ connectorSet, kilowattsSet, powerSet, statusSet });
+  return { connectorSet, kilowattsSet, powerSet, statusSet };
 }
 
-let filteredStations = filterStations({ numOfStations: 8, availability: 0.5 });
-// populateStations(filteredStations, MAP);
+async function filter() {
+  let p = {
+    searchTerm: $("#searchTerm").val(),
+    connector: parseInt($("#filter_power option:selected").val()),
+    kilowatts: parseInt($("#kilowatts_range").val()),
+    power: parseInt($("#filter_power option:selected").val()),
+    status: $("#filter_status").val(),
+  };
+
+  let params = {};
+  for (const [key, value] of Object.entries(p)) {
+    console.log(`${key}: ${value}`);
+    if (value) params[key] = value;
+  }
+
+  console.log({ params });
+  let filteredStations = await filterStations(params);
+  populateStations(filteredStations, map);
+}
+
+function updateSliderDisplayValue() {
+  $("#kw-slider-val").text($("#kilowatts_range").val());
+}
