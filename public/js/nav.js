0dd9328d0540d7dc0c7ca054a8394c6f3c2f0188
Oscar Zhu <oscar@yvrmail.net>
2022-05-24 02:50:59 -0700

    created recents modal container

--- public/js/nav.js
+++ public/js/nav.js
@@ -5,36 +5,50 @@ function openHamburgerMenu() {
     } else {
       x.style.display = "block";
     }
-  }
+}
 
-  // Search modal Functionality
-  var modal = document.getElementById("modal-search");
-  var btn = document.getElementById("openSearchModal");
-  var span = document.getElementsByClassName("searchClose")[0];
-  btn.onclick = function () {
-    modal.style.display = "block";
-  }
-  span.onclick = function () {
+// Search modal Functionality
+var modal = document.getElementById("modal-search");
+var btn = document.getElementById("openSearchModal");
+var span = document.getElementsByClassName("searchClose")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+span.onclick = function () {
+  modal.style.display = "none";
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
     modal.style.display = "none";
   }
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+}
 
-  //Filter modal Functionality
-  var modal2 = document.getElementById("filterModal");
-  var btn2 = document.getElementById("openfilterModal");
-  var span2 = document.getElementsByClassName("closefilterModal")[0];
-  btn2.onclick = function () {
-    modal2.style.display = "block";
-  }
-  span2.onclick = function () {
+//Filter modal Functionality
+var modal2 = document.getElementById("filterModal");
+var btn2 = document.getElementById("openfilterModal");
+var span2 = document.getElementsByClassName("closefilterModal")[0];
+btn2.onclick = function () {
+  modal2.style.display = "block";
+}
+span2.onclick = function () {
+  modal2.style.display = "none";
+}
+window.onclick = function (event) {
+  if (event.target == modal2) {
     modal2.style.display = "none";
   }
-  window.onclick = function (event) {
-    if (event.target == modal2) {
-      modal2.style.display = "none";
-    }
+}
+
+// Recents Modal Functionality
+const recentsModal = document.getElementById("modal-recents");
+const recentsBtn = document.getElementById("openRecentsModal");
+
+recentsBtn.onclick = function () {
+  recentsModal.style.display = "block";
+}
+
+window.onclick = function (event) {
+  if (event.target == recentsModal) {
+    recentsModal.style.display = "none";
   }
+}
