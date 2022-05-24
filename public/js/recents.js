b525acc1f6afaca7a8a2fdd466e34f706dc7c840
Oscar Zhu <oscar@yvrmail.net>
2022-05-23 19:13:52 -0700

    added example response

--- public/js/recents.js
+++ public/js/recents.js
@@ -11,4 +11,29 @@ async function openRecents() {
     }
     console.log(recentStations)
     console.log(savedStations)
+
+    // [
+    //     { userID: 74, stationID: 249385 },
+    //     { userID: 74, stationID: 2802 },
+    //     { userID: 74, stationID: 315726 },
+    //     { userID: 74, stationID: 230717 },
+    //     { userID: 74, stationID: 68322 }
+    // ]
+    // [
+    //     {
+    //       userID: 74,
+    //       stationID: 161024,
+    //       stationName: 'E 44th Ave Parking/Charging'
+    //     },
+    //     {
+    //       userID: 74,
+    //       stationID: 2802,
+    //       stationName: 'E 53rd Ave Parking Lot'
+    //     },
+    //     {
+    //       userID: 74,
+    //       stationID: 249385,
+    //       stationName: 'Springs At Langara (Coming Soon)'
+    //     }
+    // ]
 };
